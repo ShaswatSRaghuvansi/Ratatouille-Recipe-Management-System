b2aaaf329ecf5fb21efcd46d02c083a53454f7b8
ShaswatSRaghuvansi <shaswat.sr13@gmail.com>
2023-07-23 13:14:41 +0530

    Day - 5

--- Ratatouille.RMS/src/main/java/com/masaischool/Ratatouille/RMS/service/RecipeSerImp.java
+++ Ratatouille.RMS/src/main/java/com/masaischool/Ratatouille/RMS/service/RecipeSerImp.java
@@ -2,12 +2,12 @@ package com.masaischool.Ratatouille.RMS.service;
 
 import java.util.List;
 
-import com.masai.dao.RecipeDao;
-import com.masai.dao.RecipeDaoImp;
-import com.masai.entity.Recipe;
-import com.masai.entity.User;
-import com.masai.exception.NoRecordFoundException;
-import com.masai.exception.SomeThingWentWrongException;
+import com.masaischool.Ratatouille.RMS.dao.RecipeDao;
+import com.masaischool.Ratatouille.RMS.dao.RecipeDaoImp;
+import com.masaischool.Ratatouille.RMS.entity.Recipe;
+import com.masaischool.Ratatouille.RMS.entity.User;
+import com.masaischool.Ratatouille.RMS.exception.NoRecordFoundException;
+import com.masaischool.Ratatouille.RMS.exception.SomeThingWentWrongException;
 
 public class RecipeSerImp implements RecipeSer{
 
